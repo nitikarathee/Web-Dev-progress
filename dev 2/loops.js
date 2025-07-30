51f056812f1ffd66281ed63e206e53de7f2cbd05
nitikarathee <nitikarathi3535@gmail.com>
2025-07-30 15:54:54 +0530

    Add files via upload
    Today I learned about conditionals nd loop 
    (if else , switch , for/while , map)

---
+++ dev 2/loops.js
@@ -0,0 +1,30 @@
+// for 
+
+    
+// for (let i= 0;i<23 ;i++){
+//     const n =i;
+//     console.log(n);
+//     if(n==11){
+//         console.log("its my birthday");
+//     }
+
+
+// }
+// for (let i =0; i<4 ;i ++){
+//     for (let j=0; j<= 6 ; j++){
+//         console.log(` 
+//             ineer value is  ${j}`);
+//     }
+// }
+// break
+for(let i = 1 ;i<13;i++){
+    if(i == 2){
+        console.log("detected 11");
+        break;
+        
+    }
+    console.log (i);
+    return 0;
+}
+
+
