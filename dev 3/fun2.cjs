f61de4f3df6adc040e85f630099b782cadfac5e8
nitikarathee <nitikarathi3535@gmail.com>
2025-07-31 17:30:23 +0530

    Add files via upload
    Today I learned about functions and global and local scope in java

---
+++ dev 3/fun2.cjs
@@ -0,0 +1,15 @@
+// function x  (val1 ,val2, ...num1 ){
+//     return num1 
+// }
+// console.log(x(200, 300, 45))     
+
+const user ={
+    username : "hitesh",
+    price : 199
+}
+function handleobject (anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+
+}
+handleobject(user)
+handleobject(user)
---
+++ dev 3/functions.js
@@ -0,0 +1,16 @@
+// function sayMyName(){ 
+// console.log("i");
+// console.log("l");
+// console.log("u");
+// }
+// sayMyName( )
+
+//add two number 
+// const add = ( a , b ) => a + b ;
+// console.log(add(2,3));7
+function add(num1 ,num2) {
+    console.log(num1 +num2 );
+    
+}
+add(4,67);//both are number output = 71
+add(2,"200");//it thought its a string 
